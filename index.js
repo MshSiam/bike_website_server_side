ba279e6da63657a556c02d30d6a52950554d2d7a
MshSiam <siyamkenway@gmail.com>
2021-11-12 13:08:50 +0600

    40% done

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const cors = require("cors")
 require("dotenv").config()
 const { MongoClient } = require("mongodb")
 const port = process.env.PORT || 5000
+const ObjectId = require("mongodb").ObjectId
 
 // middleWere
 app.use(cors())
+app.use(express.json())
 
 const uri = ` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.woosd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 `
@@ -22,6 +24,30 @@ async function run() {
   try {
     await client.connect()
     // console.log("db connected")
+    const database = client.db("bike_website")
+    const bikeCollection = database.collection("bikes")
+    const orderCollection = database.collection("orders")
+
+    //--------GET api (bikes)------- //
+    app.get("/bikes", async (req, res) => {
+      const cursor = bikeCollection.find({})
+      const bikes = await cursor.toArray()
+      res.send(bikes)
+    })
+    // Get Single Bike
+    app.get("/bikes/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const bike = await bikeCollection.findOne(query)
+      res.json(bike)
+    })
+    // post bikes
+    app.post("/purchasing", async (req, res) => {
+      const order = req.body
+      console.log("hit api", order)
+      const result = await orderCollection.insertOne(order)
+      res.json(result)
+    })
   } finally {
     //   await client close()
   }
